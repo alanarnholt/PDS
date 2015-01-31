04bab8930c8341335e99235de8ac59d586da3a66
Alan Arnholt <arnholtat@appstate.edu>
2015-01-31 10:54:58 -0500

    first local commit

---
+++ R/PDS.R
@@ -0,0 +1,58 @@
+#' The PDS Package
+#' 
+#' Data sets for \emph{Passion Driven Statistics}
+#' @name PDS-package
+#' @docType package
+#' @title The PDS Package
+#' @keywords package
+NULL
+#####################################################################################
+#' @name AddHealth
+#' @title Adolescent to Adult Health 
+#' @aliases AddHealth
+#' @docType data
+#' @description The National Longitudinal Study of Adolescent to Adult Health (Add Health) is a longitudinal study of a nationally representative sample of adolescents in grades 7-12 in the United States during the 1994-95 school year. 
+#' @format A data frame with 6504 observations on 2829 variables
+#' @source \url{http://www.cpc.unc.edu/projects/addhealth}
+#' @details See the code books in inst/CodeBooks for details
+#' @examples
+#' dim(AddHealth)
+#' @keywords datasets
+"AddHealth"
+#####################################################################################
+#' @name gapminder
+#' @title Country level statistics
+#' @aliases gapminder
+#' @docType data
+#' @description Country level statistics
+#' @source \url{gapminder.org}
+#' @details See the code books in inst/CodeBooks for details
+#' @examples
+#' dim(gapminder)
+#' @keywords datasets
+"gapminder"
+#####################################################################################
+#' @name marscrater_pds
+#' @title Mars Craters
+#' @aliases marscrater_pds
+#' @docType data
+#' @description marscrater
+#' @format A data frame with 384343 observations on 10 variables
+#' @details See the code books in inst/CodeBooks for details
+#' @examples
+#' dim(marscrater_pds)
+#' @keywords datasets
+"marscrater_pds"
+#####################################################################################
+#' @name NESARC
+#' @title National Epidemiologic Survey on Alcohol and Related Conditions
+#' @aliases NESARC
+#' @docType data
+#' @description NESARC
+#' @format A data frame with 43093 observations on 3008 variables
+#' @details See the code books in inst/CodeBooks for details
+#' @source \url{http://pubs.niaaa.nih.gov/publications/AA70/AA70.htm}
+#' @examples
+#' dim(NESARC)
+#' @keywords datasets
+"NESARC"
